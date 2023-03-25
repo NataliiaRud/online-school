5e8ba7a70ca824c55241089f3c290815f625e328
NataliiaRud <nrud1981@gmail.com>
2023-03-25 19:56:44 +0200

    hw48 done

--- online-school/src/main/java/ua/study/school/repository/CourseRepository.java
+++ online-school/src/main/java/ua/study/school/repository/CourseRepository.java
@@ -102,18 +102,6 @@ public class CourseRepository implements BaseRepository<Course> {
         return null;
     }
 
-    /*
-CREATE PROC get_records_from_table
-    @table_name VARCHAR(50)
-AS
-BEGIN
-    SELECT *
-    FROM @table_name
-END
-     */
-
-    // https://www.digitalocean.com/community/tutorials/callablestatement-in-java-example
-
     @Override
     public List<Course> getAll() {
         try (Connection connection = DriverManager.getConnection(url, user, password);
